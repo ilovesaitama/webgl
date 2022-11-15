b8c5f5d9b9293ba89cecb122516f9ec68d216ff9
nomorefilter <109570410+nomorefilter@users.noreply.github.com>
2022-11-16 06:35:02 +0900

    Update FlyControls.js

--- jsm/controls/FlyControls.js
+++ jsm/controls/FlyControls.js
@@ -2,7 +2,7 @@ import {
 	EventDispatcher,
 	Quaternion,
 	Vector3
-} from 'https://nomorefilter.github.io/webgl/build/three.module.js';
+} from '../../build/three.module.js';
 
 const _changeEvent = { type: 'change' };
 
